7379e2eb23cb82d5b7722020bf49e22699357ef3
spicadust <spicastre@gmail.com>
2025-03-08 17:35:47 +0100

    Update job configurations

--- generate_embedding.sh
+++ generate_embedding.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-#The partition is the queue you want to run on. standard is gpu and can be ommitted.
-#SBATCH -p gpu
 #SBATCH --job-name=evo2_embedding
-#number of independent tasks we are going to start in this script
-#SBATCH --ntasks=1
-#number of cpus we want to allocate for each program
-#SBATCH --cpus-per-task=4
-#We expect that our program should not run longer than 2 days
+# normal cpu stuff: allocate cpus, memory
+#SBATCH --ntasks=1 --cpus-per-task=8 --mem=8000M
+# we run on the gpu partition and we allocate 2 titanx gpus
+#SBATCH -p gpu --gres=gpu:A100:1
+#We expect that our program should not run longer than 4 hours
 #Note that a program will be killed once it exceeds this time!
-#SBATCH --time=2-00:00:00
-#Skipping many options! see man sbatch
-# From here on, we can start our program
+#SBATCH --time=4:00:00
 
 ./mount_erda.sh
 ./setup_env.sh

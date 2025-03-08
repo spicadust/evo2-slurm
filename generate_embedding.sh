59568b24d4aeffecf91893b4209a274172e4bf7f
spicadust <spicastre@gmail.com>
2025-03-08 17:45:31 +0100

    Update job configurations

--- generate_embedding.sh
+++ generate_embedding.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --job-name=evo2_embedding
 # normal cpu stuff: allocate cpus, memory
-#SBATCH --ntasks=1 --cpus-per-task=8 --mem=8000M
-# we run on the gpu partition and we allocate 2 titanx gpus
+#SBATCH --ntasks=1 --cpus-per-task=4 --mem=4000M
+# we run on the gpu partition and we allocate 1 A100 gpu
 #SBATCH -p gpu --gres=gpu:A100:1
 #We expect that our program should not run longer than 4 hours
 #Note that a program will be killed once it exceeds this time!

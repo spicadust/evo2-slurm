7f3762705658dc9dc1687c44c6d363a54c8d9b98
spicadust <spicastre@gmail.com>
2025-03-08 17:04:33 +0100

    Create erda scripts

---
+++ mount_erda.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+key=~/.ssh/erda.ed25519
+user=nxw782@alumni.ku.dk
+erdadir=/
+mnt=~/erda
+if [ -f "$key" ]
+then
+    mkdir -p ${mnt}
+    sshfs ${user}@io.erda.dk:${erdadir} ${mnt} -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 -o IdentityFile=${key}
+else
+    echo "'${key}' is not an ssh key"
+fi

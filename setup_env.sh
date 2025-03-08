98c88236e5e1e4606191d432454e5829b15fed67
spicadust <spicastre@gmail.com>
2025-03-08 18:13:21 +0000

    Add module loading

--- setup_env.sh
+++ setup_env.sh
@@ -5,6 +5,10 @@ set -e
 
 echo "Setting up environment..."
 
+# 0. Load modules
+module load cudnn/9.7.1.26_cuda12
+module load gcc/13.2.0
+
 # 1. Install uv
 echo "Installing uv..."
 curl -LsSf https://astral.sh/uv/install.sh | sh

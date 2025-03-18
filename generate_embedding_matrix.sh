dc13f26bc501be82eda09bb178f1d54b41838687
spicadust <spicastre@gmail.com>
2025-03-18 10:29:30 +0100

    Add .sh for generating embeddings using MATRIX phages

---
+++ generate_embedding_matrix.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=evo2_embedding
+#SBATCH -p gpu --gres=gpu:h100:1
+#SBATCH --cpus-per-task=16
+# We expect that our program should not run longer than 2 days
+# Note that a program will be killed once it exceeds this time!
+#SBATCH --time=2-00:00:00
+
+export LD_LIBRARY_PATH="$HOME/lib64:$LD_LIBRARY_PATH"
+export LD_PRELOAD="/opt/software/gcc/13.2.0/lib64/libstdc++.so.6"
+
+./mount_erda.sh
+./setup_env.sh
+uv run generate-embedding --input ~/erda/CCRP/MATRIX/Analyses/Microbiomics/Ref_Genomics/phages/vOTUs_MATRIX_01_2024.fasta --output_dir ~/erda/embeddings_matrix/ --batch_size 8
+./unmount_erda.sh

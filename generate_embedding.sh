16a608e6bdcb736d24f29fd4abd2e807d2a66a01
Xinlu Shi <152381080+spicadust@users.noreply.github.com>
2025-03-20 14:04:09 +0100

    Use evo2_7b

--- generate_embedding.sh
+++ generate_embedding.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=evo2_embedding
-#SBATCH -p gpu --gres=gpu:h100:2
+#SBATCH -p gpu --gres=gpu:h100:1
 #SBATCH --cpus-per-task=16
 # We expect that our program should not run longer than 2 days
 # Note that a program will be killed once it exceeds this time!
@@ -11,5 +11,5 @@ export LD_PRELOAD="/opt/software/gcc/13.2.0/lib64/libstdc++.so.6"
 
 ./mount_erda.sh
 ./setup_env.sh
-uv run generate-embedding --input ~/erda/llm_matrix/millard_phage_Jan2025_cleaned.fa --output_dir ~/erda/embeddings/ --batch_size 2 --model_name evo2_40b --layer_name blocks.44.mlp.l3
+uv run generate-embedding --input ~/erda/llm_matrix/millard_phage_Jan2025_cleaned.fa --output_dir ~/erda/embeddings/ --batch_size 2
 ./unmount_erda.sh
